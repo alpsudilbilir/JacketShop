140ac45dc6f7fcca8591ff8a8f583d32ac86e223
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-02-27 02:53:00 +0300

    Initial commit.

---
+++ JacketShop/JacketShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  JacketShopApp.swift
+//  JacketShop
+//
+//  Created by Alpsu Dilbilir on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct JacketShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
