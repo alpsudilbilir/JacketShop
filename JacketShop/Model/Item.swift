c45d713ad8cd4c674ff4d15d6a64643edde4827d
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-02-27 03:12:32 +0300

    Model updated.

--- JacketShop/Model/Item.swift
+++ JacketShop/Model/Item.swift
@@ -23,5 +23,6 @@ let itemList: [Item] = [Item(name: "Denim Jacket", image: "jacket1", price: 49),
                         Item(name: "Blue Denim", image: "jacket8", price: 44),
                         Item(name: "Pink Palto", image: "jacket9", price: 129),
                         Item(name: "Dark Coat", image: "jacket10", price: 149),
-                        Item(name: "Yellow Coat", image: "jacket11", price: 87)
+                        Item(name: "Yellow Coat", image: "jacket11", price: 87),
                         Item(name: "Dark Brown Jacket", image: "jacket12", price: 62)]
+
