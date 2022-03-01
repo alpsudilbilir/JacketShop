91aab6ae129f161ee4798ff38a047511d3f5e65f
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-03-02 00:33:16 +0300

    Some fixes has been made.

--- JacketShop/Views/ItemRowView.swift
+++ JacketShop/Views/ItemRowView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ItemRowView: View {
+    @EnvironmentObject var vM: ViewModel
+    var item: Item
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(item.image)
+                .resizable()
+                .frame(width: 100, height: 120)
+                .cornerRadius(10)
+            VStack(alignment: .leading, spacing: 10) {
+                Text(item.name)
+                    .bold()
+                Text("$\(item.price)")
+            }
+            Spacer()
+            Image(systemName: "trash")
+                .onTapGesture {
+                    vM.removeItem(item: item)
+                }
+                .padding(.horizontal)
+        }
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
     }
 }
 
 struct ItemRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemRowView()
+        ItemRowView(item: itemList[0])
     }
 }
