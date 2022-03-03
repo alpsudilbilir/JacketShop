b02beff7bfd29ea4896204b2c7925c5d0e2ef9cb
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-03-03 17:03:19 +0300

    Some fixes has been made.

--- JacketShop/Views/DetailsView.swift
+++ JacketShop/Views/DetailsView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct DetailsView: View {
+    @EnvironmentObject var vM: ViewModel
+    var item: Item
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 5) {
+            ZStack {
+                Image(item.image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            .cornerRadius(5)
+            HStack {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(item.name)
+                        .font(.system(size: 44))
+                    Text("$\(item.price)")
+                        .font(.title2)
+                        .bold()
+                }
+                Spacer()
+                Button {
+                    vM.addToCart(item: item)
+                } label: {
+                    Image(systemName: "cart")
+                        .foregroundColor(.primary)
+                        .frame(width: 66, height: 66)
+                        .background(.secondary)
+                        .clipShape(Circle())
+                }
+            }
+            .padding()
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailsView()
+        DetailsView(item: itemList[8])
+            .preferredColorScheme(.dark)
     }
 }
